47904818e4f27aad70eecb30723bbd7d95d15fc9
Andrew-Cua <andrew@cuafamily.com>
2019-08-21 16:57:21 -0400

    Added teleop functions into drivetrain class

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -9,8 +9,10 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.ControlType;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.Utils.Constants;
 
 /**
  * Add your docs here.
@@ -18,16 +20,30 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class Drivetrain extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
-  private CANPIDController mMasterLeft;
-  private CANPIDController mFollowerLeft;
-  private CANPIDController mMasterRight;
-  private CANPIDController mFollowerRight;
+
+  private CANSparkMax mMasterLeft;
+  private CANSparkMax mLeftSlaveA;
+  private CANSparkMax mLeftSlaveB;
+  private CANSparkMax mMasterRight;
+  private CANSparkMax mRightSlaveA;
+  private CANSparkMax mRightSlaveB;
+
+
+  private CANPIDController mPIDLeft;
+  private CANPIDController mPIDRight;
 
   private static Drivetrain instance;
 
   private Drivetrain()
   {
+    mLeftSlaveA.follow(mMasterLeft);
+    mLeftSlaveB.follow(mMasterLeft);
+    mRightSlaveA.follow(mMasterRight);
+    mRightSlaveB.follow(mMasterRight);
 
+    mMasterRight.setInverted(true);
+    mRightSlaveA.setInverted(true);
+    mRightSlaveB.setInverted(true);
   }
 
  /**
@@ -39,6 +55,19 @@ public class Drivetrain extends Subsystem {
       instance = new Drivetrain();
     }
     return instance;
+  }
+
+
+  public void cartesianDrive(double x, double y)
+  {
+    double tempX = (Constants.kDriveSensFactor*Math.pow(x,3)) + ((1 - Constants.kDriveSensFactor)*x);
+    double tempY = (Constants.kDriveSensFactor*Math.pow(y,3)) + ((1 - Constants.kDriveSensFactor)*y);
+
+    double powerLeft  = tempY - tempX;
+    double powerRight = tempY + tempX;
+
+    mPIDLeft.setReference(powerLeft, ControlType.kDutyCycle);
+    mPIDRight.setReference(powerRight, ControlType.kDutyCycle);
   }
   @Override
   public void initDefaultCommand() {
